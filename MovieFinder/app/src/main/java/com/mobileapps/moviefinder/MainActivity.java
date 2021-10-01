5323d510858cecbc1cfd81c69db388c3ff937b6f
Brian Hartmann <bjhartmann13@gmail.com>
2021-10-01 19:00:29 -0400

    added account functionality

--- MovieFinder/app/src/main/java/com/mobileapps/moviefinder/MainActivity.java
+++ MovieFinder/app/src/main/java/com/mobileapps/moviefinder/MainActivity.java
@@ -3,12 +3,26 @@ package com.mobileapps.moviefinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import java.util.*;
+
+import com.google.firebase.firestore.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "DocSnippets";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
