ffa689aea7f0db6e6f4e9d6f64001210085ac345
Alyssa Adcock <alyssaadcock@Alyssas-MacBook-Air.local>
2021-10-20 16:58:20 -0400

    got the menu toolbar working

--- MovieFinder/app/src/main/java/com/mobileapps/moviefinder/FindMovieActivity.java
+++ MovieFinder/app/src/main/java/com/mobileapps/moviefinder/FindMovieActivity.java
@@ -2,7 +2,7 @@ package com.mobileapps.moviefinder;
 
 import androidx.fragment.app.Fragment;
 
-public class FindMovieActivity extends SingleFragmentActivity {
+public class FindMovieActivity extends SingleFragmentAppBarActivity {
     @Override
     protected Fragment createFragment() {
         return new FindMovieFragment();
